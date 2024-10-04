f128349d220b696655458987463c00b17913c43e
carlitospro2003 <carloseduardorsaucedo2003@gmail.com>
2024-10-03 19:43:48 -0600

    QuiboFrontend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ChatsComponent } from './pages/chats/chats.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "", component:LoginComponent
+    },
+
+    {
+        path:"register", component:RegisterComponent
+    },
+
+    {
+        path:"chats", component:ChatsComponent
+    }
+];
