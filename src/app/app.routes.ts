4120517be82e0fb97bc3055e1d9295fa74745012
carlitospro2003 <carloseduardorsaucedo2003@gmail.com>
2024-10-04 19:21:28 -0600

    cambios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -14,5 +14,6 @@ export const routes: Routes = [
 
     {
         path:"chats", component:ChatsComponent
-    }
+    },
+    
 ];
---
+++ src/app/services/chat.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { Stomp } from '@stomp/stompjs';
+import  SockJS from 'sockjs-client';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+
+  constructor() { }
+
+
+
+}
