16484e3ce9951a74e75dd76d1da57688e7e2d879
arkadiem <jasonusotelo@gmail.com>
2024-10-08 21:15:27 -0600

    chats 1/4

--- src/app/pages/chats/chats.component.ts
+++ src/app/pages/chats/chats.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import {FormsModule} from "@angular/forms";
+import { HttpClientModule, HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-chats',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule,
+    HttpClientModule,
+    FormsModule
+
+  ],
   templateUrl: './chats.component.html',
   styleUrl: './chats.component.css'
 })
 export class ChatsComponent {
+  message: string ='';
 
+  constructor(private http: HttpClient, private router: Router) { }
+
+  onSubmit() {
+    this.http.post('http://localhost:8000/api/auth/message',{message : this.message})
+      .subscribe()
+    console.log('Pase?????')
+  }
 }
+
+const getUsername = async () => {
+  let username = localStorage.getItem('username');
+  while (!username) {
+    username = prompt('Please enter your username');
+    if (username) {
+      localStorage.setItem('username', username);
+    } else {
+      alert('Username cannot be empty. Please enter a valid username.');
+    }
+  }
+  return username;
+};
